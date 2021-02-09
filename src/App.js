b55b2dde571bba9d1953504e3623aab6cbc41f3f
Christian <christian.jhardy.steinmann@gmail.com>
2021-02-09 07:21:46 +0200

    added site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from "react";
-import {Products, Navbar, Cart, Checkout } from "./components";
+import {Products, Navbar, Cart, Checkout} from "./components";
 import {commerce} from "./lib/commerce";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import LoginButton from "./components/Buttons/LoginButton";
+import LogoutButton from "./components/Buttons/LogoutButton";
+import Profile from "./components/Profile/Profile";
+import {useAuth0} from "@auth0/auth0-react";
 
 const App = () => {
     const [products, setProducts] = useState([]);
@@ -18,23 +22,23 @@ const App = () => {
         return data;
     }
     const handleAddToCart = async (productId, quantity) => {
-        const { cart } = await commerce.cart.add(productId, quantity);
+        const {cart} = await commerce.cart.add(productId, quantity);
         setCart(cart);
     }
 
     const handleUpdateCartQty = async (productId, quantity) => {
-        const { cart } = await commerce.cart.update(productId, {quantity});
+        const {cart} = await commerce.cart.update(productId, {quantity});
         setCart(cart);
     }
 
     const handleRemoveFromCart = async (productId) => {
-        const { cart } = await commerce.cart.remove(productId);
+        const {cart} = await commerce.cart.remove(productId);
 
         setCart(cart);
     }
 
     const handleEmptyCart = async () => {
-        const { cart } = await commerce.cart.empty();
+        const {cart} = await commerce.cart.empty();
 
         setCart(cart);
     }
@@ -49,7 +53,7 @@ const App = () => {
             const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
             setOrder(incomingOrder);
             refreshCart();
-        }catch (error) {
+        } catch (error) {
             setErrorMessage(error.data.error.message);
         }
     }
@@ -64,34 +68,49 @@ const App = () => {
         ));
     }, []);
 
-    return (
-        <Router>
-            <div>
-                <Navbar totalItems={cart?.total_items ?? 0}/>
-                <Switch>
-                    <Route exact path="/">
-                        <Products products={products} onAddToCart={handleAddToCart}/>
-                    </Route>
-                    <Route exact path="/cart">
-                        {cart && <Cart
-                            cart={cart}
-                            handleUpdateCartQty={handleUpdateCartQty}
-                            handleRemoveFromCart={handleRemoveFromCart}
-                            handleEmptyCart={handleEmptyCart}
-                        />}
-                    </Route>
-                    <Route exact path="/checkout">
-                        <Checkout
-                            cart={cart}
-                            order={order}
-                            onCaptureCheckout={handleCaptureCheckout}
-                            error={errorMessage}
-                        />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+    const {isLoading, isAuthenticated} = useAuth0();
 
+    if (isLoading) return <>Loading...</>
+
+    if (!isAuthenticated) return <LoginButton />
+
+    return (
+        isAuthenticated && (
+            <Router>
+
+                <div>
+                    <Navbar totalItems={cart?.total_items ?? 0}/>
+                    <Switch>
+                        <Route exact path="/">
+                            <Products products={products} onAddToCart={handleAddToCart}/>
+                        </Route>
+                        <Route exact path="/profile">
+                            <Profile/>
+                        </Route>
+                        <Route exact path="/logout">
+                            <LogoutButton/>
+                        </Route>
+                        <Route exact path="/cart">
+                            {cart && <Cart
+                                cart={cart}
+                                handleUpdateCartQty={handleUpdateCartQty}
+                                handleRemoveFromCart={handleRemoveFromCart}
+                                handleEmptyCart={handleEmptyCart}
+                            />}
+                        </Route>
+                        <Route exact path="/checkout">
+                            <Checkout
+                                cart={cart}
+                                order={order}
+                                onCaptureCheckout={handleCaptureCheckout}
+                                error={errorMessage}
+                            />
+                        </Route>
+
+                    </Switch>
+                </div>
+            </Router>
+        )
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN_KEY;
+const clientIdKey = process.env.REACT_APP_AUTH0_CLIENT_ID_KEY;
+
+ReactDom.render(
+    <Auth0Provider
+    domain={domain}
+    clientId={clientIdKey}
+    redirectUri={window.location.origin}>
+        <App />
+    </Auth0Provider>
+    , document.getElementById("root"));
